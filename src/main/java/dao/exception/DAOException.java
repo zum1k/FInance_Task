f97c847abfd6ef8e9add8aeac4adea66afea2675
Your Name <you@example.com>
2020-03-12 14:48:12 +0300

    .

--- src/main/java/dao/exception/DAOException.java
+++ src/main/java/dao/exception/DAOException.java
@@ -1,6 +1,6 @@
 package dao.exception;
 
-public class DAOException extends RuntimeException {
+public class DAOException extends Exception {
     public DAOException(String msg, Throwable cause) {
         super(msg, cause);
     }
